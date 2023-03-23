72864ffe02f40f59fedb9e54e00549dbcc5e1071
Arata <shahzod20030905@gmail.com>
2023-03-23 23:24:46 +0100

    redesigned menu, fixed bug with emission, commands are working almost correctly

--- menu.py
+++ menu.py
@@ -1,58 +1,57 @@
+import concurrent.futures
+import aiohttp
+import asyncio
+import sys
+import os
+
 from api import BaseRequests
 from datetime import datetime
 
-import aiohttp
-import asyncio
 
 class Menu(BaseRequests):
+#TODO Do i rly need a class??
 #TODO Dont rly like how the current menu looks like.
 # Need to come up with better UI
 
-#TODO Do i rly need a class??
+#TODO data is being retrieved in json format which is not rly readable for users.
+#Need to find better way to provide data.
 
-	menu_options = """
-1:Clans
-2:Users
-3:Emission
-4:Region
-5:Auction
-6:Quit
-	"""
-	clan_options = """
-1:List of Clans
-2:Clan Members
-3:Clan information
-4:Back to menu
-"""
-	auction_options = """
-1:Active lots
-2:Item history
-3:Back to menu
-	"""
-	profile_options = """
-1:List of Characters
-2:Characters Profile
-3:Back to menu
-"""
 
+	menu_options = "1:Clans\n2:Users\
+					\n3:Emission\n4:Region\
+					\n5:Auction\n6:Quit\n>> "
+	clan_options = "1:List of Clans\n2:Clan Members\
+					\n3:Clan information\n4:Back to menu\
+					\n>> "
+	auction_options = "1:Active lots\n2:Item history\
+		\n3:Back to menu\n>> "
+	profile_options = "1:List of Characters\n2:Characters Profile\
+	\n3:Back to menu\n>> "
+
+	def create_loop(self, method ):
+		loop = asyncio.get_running_loop()
+		loop.run_in_executor(None, method)
+		return loop
 
 	async def clans(self):
-		user_input = input(f"Please select one of the options\n {self.clan_options}")
+		user_input = input(f"Please select one of the options\n{self.clan_options}")
 
 		match int(user_input):
 			case 1:
 				await self.clan_list()
 			case 2:
-				await self.clan_members()
+				clan_id = input('Please provide clans id to get information about its members \n>> ')
+				await self.clan_members(region='ru', clan_id=clan_id)
 			case 3:
-				await self.clan_info()
+				clan_id = input('Please write clans id which you want to look up\n>> ')
+				await self.clan_info(region='ru', clan_id=clan_id)
 			case 4:
 				await self.menu()
 			case _:
 				print('Wrong input! Please try again')
 
 	async def auction(self):
-		user_input = input(f"Please select one of the options\n {self.auction_options}")
+		user_input = input(f"Please select one of the options\n{self.auction_options}")
 		match int(user_input):
 			case 1:
 				await self.auction_lots()
@@ -64,7 +63,7 @@ class Menu(BaseRequests):
 				print('Wrong input! Please try again')
 
 	async def profiles(self):
-		user_input = input(f"Please select one of the options\n {self.profile_options}")
+		user_input = input(f"Please select one of the options\n{self.profile_options}")
 		match int(user_input):
 			case 1:
 				await self.list_of_chars()
@@ -81,29 +80,29 @@ class Menu(BaseRequests):
 		emission = datetime.fromisoformat(emission.replace('Z', '+00:00'))
 		emission = emission.strftime("%H:%M:%S")
 		print(f'Last emission was at : {emission}')
-		return emission
+
 
 	async def menu(self):
-		user_input = input(f'Welcome to StalcBot!\nPlease select one of the following options.\n{self.menu_options}\n')
-		# while True:
-		match int(user_input):
-			case 1:
-				await self.clans()
+		user_input = input(f'Welcome to StalcBot!\nPlease select one of the following options.\n{self.menu_options}')
+		while True:
+			match int(user_input):
+				case 1:
+					await asyncio.create_task(self.clans())
+				case 2:
+					await asyncio.create_task(self.profiles())
+				case 3:
+					await asyncio.create_task(self.last_emission())
+					await asyncio.create_task(self.menu())
+				case 4:
+					await asyncio.create_task(self.regions())
+				case 5:
+					await asyncio.create_task(self.auction())
+				case 6:
+					sys.exit(0)
+				case _:
+					print('Wrong input! Please try it again')
 
-			case 2:
-				await self.profiles()
-			case 3:
-				await self.last_emission()
-			case 4:
-				await self.regions()
-			case 5:
-				await self.auction()
-			# case 6:
-			# 	break
-			case _:
-				print('Wrong input! Please try it again')
 
 
 menu = Menu()
-# loop = asyncio.get_event_loop()
 asyncio.run(menu.menu())
