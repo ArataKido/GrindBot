ad6ee12941f9ca004d1471d0b7b824f9a6f21ab4
Arata <shahzod20030905@gmail.com>
2023-03-27 00:33:37 +0200

    compelted work with clan section + smal changes

--- api.py
+++ api.py
@@ -7,7 +7,7 @@ class BaseRequests():
 	#TODO refactor whole class and use one session for all methods
 	# instead of making new session in each method
 	headers = {
-		'Authorization': f'Bearer {USER_TOKEN}'
+		'Authorization': f'Bearer {APP_TOKEN}'
 		}
 	region = 'ru'
 	clan_id = '647d6c53-b3d7-4d30-8d08-de874eb1d845'
@@ -15,52 +15,60 @@ class BaseRequests():
 	item = 'y1q9'
 
 	async def regions(self):
-		async with aiohttp.ClientSession(DEMO_URL) as session:
+		async with aiohttp.ClientSession(PROD_URL) as session:
 			async with session.get('/regions') as response:
 				return await response.json()
 
 	async def emission(self, region):
-		async with aiohttp.ClientSession(DEMO_URL) as session:
+		async with aiohttp.ClientSession(PROD_URL) as session:
 			async with session.get(f'/{region}/emission',  headers=self.headers) as response:
 				return await response.json()
 
 
 	# Metods for getting information about clans
 	async def clan_info(self, clan_id=clan_id, region=region, ):
-		async with aiohttp.ClientSession(DEMO_URL) as session:
+		async with aiohttp.ClientSession(PROD_URL) as session:
 			async with session.get(f'/{region}/clan/{clan_id}/info', headers=self.headers) as response:
 				return await response.json()
 
 	async def clan_list(self, region=region):
-		async with aiohttp.ClientSession(DEMO_URL) as session:
+		async with aiohttp.ClientSession(PROD_URL) as session:
 			async with session.get(f'/{region}/clans/', headers=self.headers) as response:
 				return await response.json()
 
 	async def clan_members(self, region=region, clan_id=clan_id):
-		async with aiohttp.ClientSession(DEMO_URL) as session:
+		async with aiohttp.ClientSession(PROD_URL) as session:
 			async with session.get(f'/{region}/clan/{clan_id}/members', headers=self.headers) as response:
-				return await response.json()
+				if response.status != 200:
+					return 'You cannot view members of this clan because you dont have character there '
+				return response.json()
 
 	# Metods for getting information about Users characters
 
 	async def profile(self, region=region, character=character):
 		#TODO For some reason this method does work, need to check it with abvadabra
-		async with aiohttp.ClientSession(DEMO_URL) as session:
+		async with aiohttp.ClientSession(PROD_URL) as session:
 			async with session.get(f'/{region}/character/by-name/{character}/profile', headers=self.headers) as response:
 				return await response.json()
 
 	async def list_of_chars(self, region=region,):
-		async with aiohttp.ClientSession(DEMO_URL) as session:
+		async with aiohttp.ClientSession(PROD_URL) as session:
 			async with session.get(f'/{region}/characters', headers=self.headers) as response:
 				return await response.json()
 
 	# Metods for getting information about auction
 	async def auction_item_history(self, region=region, item=item):
-		async with aiohttp.ClientSession(DEMO_URL) as session:
+		async with aiohttp.ClientSession(PROD_URL) as session:
 			async with session.get(f'/{region}/auction/{item}/history', headers=self.headers) as response:
+				if response.status == 400:
+					return False
+				print(f'{await response.json()} THIS WAS API')
 				return await response.json()
 
 	async def auction_lots(self, region=region, item=item):
-		async with aiohttp.ClientSession(DEMO_URL) as session:
+		async with aiohttp.ClientSession(PROD_URL) as session:
 			async with session.get(f'/{region}/auction/{item}/lots', headers=self.headers) as response:
+				if response.status == 400:
+					return False
+				print(f'{await response.json()} THIS WAS API')
 				return await response.json()
--- menu.py
+++ menu.py
@@ -8,10 +8,39 @@ from api import BaseRequests
 from datetime import datetime
 
 
-class Menu(BaseRequests):
+class Serializers():
+#TODO Find better name for this class. Lol
+	"""Purpose of this class is to proses json which is
+		returned by api and print required information to user in most
+		readable way
+
+	"""
+	async def clan_list_serializer(self, clans:dict):
+		#TODO need to validate len of clans data
+		n = 1
+		print('============= LIST OF CLANS =============')
+		for clan in clans['data']:
+			print(n)
+			print(f"\nClan [{clan['tag']}] {clan['name']}\nMembers: {clan['memberCount']}\
+	 				\nFaction: {clan['alliance']}\nLeader: {clan['leader']}")
+			n += 1
+		print('=========================================')
+
+	async def active_lots_serializers(self, lots:dict):
+		try:
+			print(len(lots['lots']))
+			if len(lots['lots']) != 0 :
+				for lot in lots['lots']:
+					print(f"Item: {lot['itemId']}\nBet price: {lot.get('startPrice')}\nBuyout : {lot.get('buyoutPrice')}")
+				return True
+			print("There is no such item on auction!")
+		except KeyError:
+			#TODO REMOVE THIS PRINT, DONT LET NICLAS SEE THIS.
+			return print('Иди нахуй а, научись писать нормально')
+
+class Menu(BaseRequests, Serializers):
 #TODO Do i rly need a class??
-#TODO Dont rly like how the current menu looks like.
-# Need to come up with better UI
+
 
 #TODO data is being retrieved in json format which is not rly readable for users.
 #Need to find better way to provide data.
@@ -27,45 +56,68 @@ class Menu(BaseRequests):
 	\n3:Back to menu\n>> "
 
 	async def clans(self):
-		user_input = input(f"Please select one of the options\n{self.clan_options}")
+		user_input = input(f"\nPlease select one of the options\n{self.clan_options}")
 
-		match int(user_input):
-			case 1:
-				await self.clan_list()
-			case 2:
+		match user_input:
+			case '1':
+				clans =  await self.clan_list()
+				if not clans:
+					return print("Something went wrong")
+				await self.clan_list_serializer(clans=clans)
+			case '2':
 				clan_id = input('Please provide clans id to get information about its members \n>> ')
-				await self.clan_members(region='ru', clan_id=clan_id)
-			case 3:
+				if not clan_id:
+					return print('ACHTUNG!!! YOUR MESSAGE IS EMPTY!')
+				members = await self.clan_members(region='ru', clan_id=clan_id)
+			case '3':
 				clan_id = input('Please write clans id which you want to look up\n>> ')
-				await self.clan_info(region='ru', clan_id=clan_id)
-			case 4:
+				if not clan_id:
+					return print('ACHTUNG!!! YOUR MESSAGE IS EMPTY!')
+				clan = await self.clan_info(region='ru', clan_id=clan_id)
+			case '4':
 				await self.menu()
 			case _:
 				print('Wrong input! Please try again')
+				await self.clans()
 
 	async def auction(self):
-		user_input = input(f"Please select one of the options\n{self.auction_options}")
-		match int(user_input):
-			case 1:
-				await self.auction_lots()
-			case 2:
-				await self.auction_item_history()
-			case 3:
+		user_input = input(f"\nPlease select one of the options\n{self.auction_options}")
+		match user_input:
+			case '1':
+				item = input('Please enter the name of a item you are searching for \n>> ')
+				if not item:
+					return print('ACHTUNG!!! YOUR MESSAGE IS EMPTY!')
+				lots = await self.auction_lots(item=item)
+				if not lots:
+					return print('Item was not found')
+				await self.active_lots_serializers(lots=lots)
+			case '2':
+				user_input = input('Please enter the name of a item you are searching for \n>> ')
+				history = await self.auction_item_history()
+				if not history:
+					return print('Item was not found')
+				print(history)
+			case '3':
 				await self.menu()
 			case _:
 				print('Wrong input! Please try again')
+				await self.auction()
 
 	async def profiles(self):
-		user_input = input(f"Please select one of the options\n{self.profile_options}")
-		match int(user_input):
-			case 1:
-				await self.list_of_chars()
-			case 2:
-				await self.profile()
-			case 3:
+		user_input = input(f"\nPlease select one of the options\n{self.profile_options}")
+		match user_input:
+			case '1':
+				chars = await self.list_of_chars()
+				print(chars)
+			case '2':
+				user_input = input('Please write characters name\n>> ')
+				profile = await self.profile(character=user_input)
+				print(profile)
+			case '3':
 				await self.menu()
 			case _:
 				print('Wrong input! Please try again')
+				await self.profiles()
 
 	async def last_emission(self, region='ru' ):
 		emission = await (self.emission(region=region))
@@ -76,26 +128,29 @@ class Menu(BaseRequests):
 
 
 	async def menu(self):
-		user_input = input(f'Welcome to StalcBot!\nPlease select one of the following options.\n{self.menu_options}')
+		#TODO Check if session is being closed
+		print('\nWelcome to StalcBot!')
+		user_input = input(f'Please select one of the following options.\n{self.menu_options}')
 		while True:
-			match int(user_input):
-				case 1:
+			match user_input:
+				case '1':
 					await asyncio.create_task(self.clans())
-				case 2:
+				case '2':
 					await asyncio.create_task(self.profiles())
-				case 3:
+				case '3':
 					await asyncio.create_task(self.last_emission())
 					await asyncio.create_task(self.menu())
-				case 4:
+				case '4':
 					await asyncio.create_task(self.regions())
-				case 5:
+				case '5':
 					await asyncio.create_task(self.auction())
-				case 6:
+				case '6':
 					sys.exit(0)
 				case _:
-					print('Wrong input! Please try it again')
+					print('\nWrong input! Please try it again\n')
+					await asyncio.create_task(self.menu())
 
 
 
 menu = Menu()
-asyncio.run(menu.menu())
+asyncio.get_event_loop().run_until_complete(menu.menu())
