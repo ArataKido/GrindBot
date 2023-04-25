2d141a8f4389472eede7fb2c955dc44a85a91b91
Arata <shahzod20030905@gmail.com>
2023-04-25 22:16:50 +0200

    moved tools to map

---
+++ utils/parce.py
@@ -0,0 +1,30 @@
+import os
+import json
+
+def get_file_links():
+	link = 'stalcraft-database-main\global\items'
+	folder_links = [folder.path for folder in os.scandir(link)]
+	sub_folder_link = []
+	for sub_folder in folder_links:
+		for folder in os.scandir(sub_folder):
+			if os.path.isfile(folder):
+				sub_folder_link.append(folder.path)
+	read_files(sub_folder_link)
+
+
+def read_files(path):
+	to_save =  {}
+	with open('utils/db.json', 'w', encoding='utf-8') as db:
+		for file in path:
+			with open(file, 'r', encoding='utf-8') as file:
+				file = json.load(file)
+				item_id = file.get('id')
+				item_name = file['name']['lines'].get('en').lower()
+				to_save[item_name] = item_id
+		json.dump(to_save, db)
+		db.close()
+
+
+
+
+get_file_links()
